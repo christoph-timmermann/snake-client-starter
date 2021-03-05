a28a318da54f2b40bae01994a3eff78bff9bf71d
christoph-timmermann <christoph.timmermann@ah-oh.com>
2021-03-05 17:30:04 +0100

    3 files modified, 1 added

--- index.js
+++ index.js
@@ -1,6 +1,58 @@
-// Import stylesheets
-import './style.css';
+import { init, login, setDirection } from "./network.js";
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+const WIDTH = 50; // Breite des Spielfelds
+const HEIGHT = 50; // Höhe des Spielfelds
+const TILE_SIZE = 16; // Größe einer Kachel in Pixeln
+
+const Direction = {
+  UP: 0,
+  RIGHT: 1,
+  DOWN: 2,
+  LEFT: 3
+};
+
+// Wenn der Netzwerk-Client verbunden hat
+addEventListener("connect", event => {
+  login("MYROOM");
+});
+
+// Wenn sich der Zustand des Spiels ändert
+addEventListener("update", event => {});
+
+let ctx;
+let initialized = false;
+// Wenn die Webseite fertig geladen wurde
+function gameInit() {
+  initialized = true;
+
+  // Mit Name verbinden
+  init("Spieler");
+
+  // Canvas
+  const canvas = document.getElementById("canvas");
+  ctx = canvas.getContext("2d");
+
+  ctx.canvas.width = WIDTH * TILE_SIZE;
+  ctx.canvas.height = HEIGHT * TILE_SIZE;
+}
+
+checkLoaded;
+function checkLoaded() {
+  if (document.getElementById("canvas") && !initialized) {
+    gameInit();
+  } else {
+    setTimeout(checkLoaded, 15);
+  }
+}
+
+// Funktionen zum Zeichnen
+function fillTile(x, y, color) {
+  ctx.beginPath();
+  ctx.rect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+  ctx.fillStyle = color;
+  ctx.fill();
+  ctx.closePath();
+}
+function clearCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
---
+++ network.js
@@ -0,0 +1,52 @@
+let webSocket;
+
+// Diese Funktion muss vor allen anderen ausgeführt werden
+export function init(name) {
+  webSocket = new WebSocket("ws://localhost:5000");
+
+  webSocket.onerror = (socket, event) => {
+    console.error(event);
+  };
+
+  webSocket.onopen = () => {
+    webSocket.send(
+      JSON.stringify({ type: "connect", name: name ? name : "Unbekannt" })
+    );
+  };
+
+  // Wenn eine Nachricht vom Server kommt
+  webSocket.onmessage = message => {
+    const data = JSON.parse(message.data);
+
+    switch (data.type) {
+      // Wenn die Verbindung hergestellt wird
+      case "connected":
+        console.log(`Connection to server successful, your id is ${data.id}.`);
+
+        const onConnect = new CustomEvent("connect", { detail: data });
+        dispatchEvent(onConnect);
+        break;
+
+      // Wenn ein Raum betreten wurde
+      case "login":
+        console.log(`You are now logged into room ${data.roomId}.`);
+        break;
+
+      // Wenn Spielerdaten empfangen wurden
+      case "update":
+        const onUpdate = new CustomEvent("update", { detail: data });
+        dispatchEvent(onUpdate);
+        break;
+    }
+  };
+}
+
+// Mit dieser Funktion kann man sich in einen Raum anmelden
+export function login(roomId) {
+  webSocket.send(JSON.stringify({ type: "login", roomId: roomId }));
+}
+
+// Mit dieser Funktion kann die Richtung geändert werden
+export function setDirection(direction) {
+  webSocket.send(JSON.stringify({ type: "direction", direction: direction }));
+}
